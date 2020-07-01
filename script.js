b749cacbba0494b6f4829176a416c368327405fb
Suman Gupta <suman.gupta@gmail.com>
2020-07-02 00:49:25 +0530

    Added infinite scrolling functionality

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let photosArray = [];
+let ready = false;
+let imagesRendered = 0;
+let totalImages = 0;
 
 function setAttributes(element, attributes) {
     for(const key in attributes) {
@@ -9,7 +12,18 @@ function setAttributes(element, attributes) {
     }
 }
 
+function imageRendered() {
+    imagesRendered++;
+    if(imagesRendered === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 function displayPhotos() {
+    imagesRendered = 0;
+    totalImages = photosArray.length;
+
     // Run function for each object in photosArray
     photosArray.forEach(photo => {
         // Create <a> to link to Unsplash
@@ -27,6 +41,9 @@ function displayPhotos() {
             title: photo.alt_description
         });
 
+        // Event Listener, check when each photo is finished loading
+        img.addEventListener('load', imageRendered);
+
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -34,7 +51,7 @@ function displayPhotos() {
 }
 
 async function getPhotos() {
-    const count = 10;
+    const count = 30;
     const apiKey = 'EIqqdYFWjQE_XHz18lxlCXuc6jQLFlqHPT0ni9aeQVE';
     const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
@@ -46,6 +63,14 @@ async function getPhotos() {
 
     }
 }
+
+// Check to see if scrolling is near bottom of the page, Load more photos
+window.addEventListener('scroll', () => {
+    if((window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
 
 // On Load
 getPhotos();
